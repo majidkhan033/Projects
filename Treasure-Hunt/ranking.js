a0e07edecd3e7c182ed457dd86c0cf59fd27aaf5
majidkhan033 <kmajid748@gmail.com>
2022-11-02 18:07:06 +0530

    Updated

--- Treasure-Hunt/ranking.js
+++ Treasure-Hunt/ranking.js
@@ -46,8 +46,8 @@ winnerData = winnerData.flat();
 winnerData.sort((a, b) => a.attempts - b.attempts);
 temp[0] = winnerData[0];
 // console.log(winnerData);
-// let i;
-for (let i = 1; i < winnerData.length; i++) {
+let i;
+for ( i = 1; i < winnerData.length; i++) {
     if (winnerData[i].attempts == winnerData[i - 1].attempts) {
         temp.push(winnerData[i]);
     } else {
